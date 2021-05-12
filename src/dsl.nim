93c01a503d6fc10d77c4c0ef8f16f08071b4dd98
Samuel Melm <samuel.melm@js-soft.com>
2021-05-12 20:33:09 +0200

    chore: remove pregenerated comments

--- src/dsl.nim
+++ src/dsl.nim
@@ -1,7 +1,3 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
-
 import dsl/db
 import sugar
 import strutils
