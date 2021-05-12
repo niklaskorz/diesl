5b4da324dcd220a45161c7a36b6b320931ed582e
Samuel Melm <samuel.melm@js-soft.com>
2021-05-12 20:38:39 +0200

    test: add test for mock db

---
+++ tests/test_db.nim
@@ -0,0 +1,16 @@
+
+
+import unittest
+import dsl/db
+import sugar
+import strutils
+
+test "can map over string columns":
+    let dbTable = newDBTable(
+        newStringColumn(name = "text", data = @["foo bar", "baz bar"])
+    )
+
+    let actual = dbTable.text.map(str => str.split(" ")[0])
+    let expected = newStringColumn(name = "text", data = @["foo", "baz"])
+
+    check actual == expected
