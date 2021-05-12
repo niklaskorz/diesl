f70f06b124aab59a18e33d4d8f9d1f4cfe4bbeff
Samuel Melm <samuel.melm@js-soft.com>
2021-05-12 22:28:17 +0200

    test: trim function

---
+++ tests/test_string.nim
@@ -0,0 +1,29 @@
+
+import unittest
+import dsl
+import dsl/db
+
+
+let dbTable = newDBTable(
+    newStringColumn(name = "text", data = @["  foo", "  bar  ", "baz  "])
+)
+
+test "trim left":
+    let actual = dbTable.text.trim(trimLeft)
+    let expected = newStringColumn(name = "text", data = @["foo", "bar  ", "baz  "])
+
+    check actual == expected
+
+
+test "trim right":
+    let actual = dbTable.text.trim(trimRight)
+    let expected = newStringColumn(name = "text", data = @["  foo", "  bar", "baz"])
+
+    check actual == expected
+
+
+test "trim both":
+    let actual = dbTable.text.trim(trimBoth)
+    let expected = newStringColumn(name = "text", data = @["foo", "bar", "baz"])
+
+    check actual == expected
