ae87d3fac4f683a7691fc2af7d10075fd7001562
Samuel Melm <samuel.melm@js-soft.com>
2021-05-14 12:39:59 +0200

    chore: do something sensible with dsl.nim

--- src/dsl.nim
+++ src/dsl.nim
@@ -1,11 +1,4 @@
 import dsl/[db, language]
 
-
-when isMainModule:
-  var dbTable = newDBTable(
-      newStringColumn(name = "text", data = @["  foo", "  bar  ", "baz  "])
-  )
-  
-  echo dbTable.text
-  echo dbTable.text.trim(trimBoth)
-
+export db
+export language
