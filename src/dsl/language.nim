7fd7ce701de39ab23fddc9fdc6aeb527a604c00d
Samuel Melm <samuel.melm@js-soft.com>
2021-05-14 09:46:47 +0200

    feat: implement trim function

---
+++ src/dsl/language.nim
@@ -0,0 +1,12 @@
+
+import db
+import sugar
+import strutils
+
+type TrimDirection* = enum trimLeft, trimRight, trimBoth
+
+proc trim*(column: StringColumn, direction: TrimDirection = trimBoth) : StringColumn = 
+    let leading = direction == trimBoth or direction == trimLeft
+    let trailing = direction == trimBoth or direction == trimRight
+
+    return column.map(str => str.strip(leading = leading, trailing = trailing))
