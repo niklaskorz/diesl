e9737b59992dd63d282483d003685c43d419a829
Niklas Korz <niklas@niklaskorz.de>
2021-05-31 10:34:51 +0200

    Solve t4swe

--- swe_exercises/t4swe/ExerciseManager/exercise_mgr/populate/3_prepare_files.py
+++ swe_exercises/t4swe/ExerciseManager/exercise_mgr/populate/3_prepare_files.py
@@ -13,7 +13,7 @@ def main():
         print("lyx is not installed or not accessible, aborting")
         sys.exit(1)
 
-    paths = [settings.files / str(i) for i in range(19, 20)]
+    paths = [settings.files / str(i) for i in range(19, 21)]
     for path in paths:
         files = path.glob("*.lyx")
         (path / "comment").mkdir(exist_ok=True)
@@ -22,10 +22,10 @@ def main():
             lyx_to_latex.notes_to_comments(file, (path / "comment" / file.name))
             lyx_to_latex.lyx_to_latex(str(path / "comment" / file.name))
             input_path = str(path / "comment" / (file.stem + ".tex"))
-            out_folder = path / "split"
+            out_path = path / "split" / (file.stem + ".tex")
             iter_lines = read_problemset(input_path)
             iter_exercises = split_problemset_into_header_and_exercises(iter_lines)
-            save_header_and_exercises(input_path, out_folder, iter_exercises)
+            save_header_and_exercises(input_path, out_path, iter_exercises)
         print(path)
 
 
