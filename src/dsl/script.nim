efaf2f9b5079fedefd15bc814c36f349017a07a9
Niklas Korz <niklas@niklaskorz.de>
2021-06-10 18:52:45 +0200

    Dynamically detect Nim standard library path (closes #33)

--- src/dsl/script.nim
+++ src/dsl/script.nim
@@ -1,7 +1,24 @@
 import os
 import nimscripter
 
+type StdPathNotFoundException* = object of Exception
+
+proc getStdPath*(): string =
+  # User defined path to standard library
+  var stdPath = os.getEnv("NIM_STDLIB")
+  # Fallback to current directory version of stdlib
+  if stdPath == "" and dirExists(getCurrentDir() / "stdlib"):
+    stdPath = getCurrentDir() / "stdlib"
+  # Fallback to stdlib of current Nim compiler
+  if stdPath == "":
+    let home = getHomeDir()
+    stdPath = home / ".choosenim" / "toolchains" / ("nim-" & NimVersion) / "lib"
+
+  if not dirExists(stdPath):
+    raise StdPathNotFoundException.newException("No standard library found at path " & stdPath)
+  return stdPath
+
+
 proc runScript*(script: string): Option[Interpreter] =
-  let home = os.getEnv("HOME")
-  let stdPath = home / ".choosenim" / "toolchains" / "nim-1.4.6" / "lib"
+  let stdPath = getStdPath()
   return loadScript(script, isFile = false, stdPath = stdPath)
