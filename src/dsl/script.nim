8f176ca48e6731980f59545c1aacb0a61f498b60
Niklas Korz <niklas@niklaskorz.de>
2021-06-11 12:53:23 +0200

    Adjust standard library detection for docker container

--- src/dsl/script.nim
+++ src/dsl/script.nim
@@ -7,16 +7,25 @@ type StdPathNotFoundException* = object of Defect
 proc getStdPath*(): string =
   # User defined path to standard library
   var stdPath = os.getEnv("NIM_STDLIB")
+  if stdPath != "" and not dirExists(stdPath):
+    raise StdPathNotFoundException.newException("No standard library found at path " & stdPath)
+
   # Fallback to current directory version of stdlib
-  if stdPath == "" and dirExists(getCurrentDir() / "stdlib"):
-    stdPath = getCurrentDir() / "stdlib"
-  # Fallback to stdlib of current Nim compiler
   if stdPath == "":
+    stdPath = getCurrentDir() / "stdlib"
+
+  # Fallback to stdlib of current Nim compiler
+  if not dirExists(stdPath):
     let home = getHomeDir()
     stdPath = home / ".choosenim" / "toolchains" / ("nim-" & NimVersion) / "lib"
 
+  # Fallback to nim lib in docker image
   if not dirExists(stdPath):
-    raise StdPathNotFoundException.newException("No standard library found at path " & stdPath)
+    stdPath = "/nim/lib"
+
+  if not dirExists(stdPath):
+    raise StdPathNotFoundException.newException("No standard library found, please set NIM_STDLIB environment variable")
+
   return stdPath
 
 
