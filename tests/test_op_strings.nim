ece2e222a5df17624b2b053c1330afb78be6c941
Benjamin Sparks <benjamin.sparks@protonmail.com>
2021-06-27 20:09:06 +0200

    test: check replacing

--- tests/test_op_strings.nim
+++ tests/test_op_strings.nim
@@ -14,5 +14,22 @@ proc test_strings*() =
       check substringOp.kind == dotSubstring
       check substringOp.substringRange == 0..5
 
+
+    test "replace":
+      let db = Diesl()
+      db.table.to = db.table.frm.replace("old".toOperation, "new".toOperation)
+
+      let ops = db.exportOperations()
+      let storeOp = ops[0]
+
+      let replacementOp = storeOp.storeValue
+      check replacementOp.kind == dotReplace
+
+      check replacementOp.replaceTarget.kind == dotStringLiteral
+      check replacementOp.replaceTarget.stringValue == "old"
+
+      check replacementOp.replaceReplacement.kind == dotStringLiteral
+      check replacementOp.replaceReplacement.stringValue == "new"
+
 when isMainModule:
   test_strings()
