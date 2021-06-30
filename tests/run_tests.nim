f22dc8bcda0f329a78d3f802a4676f600048a066
Benjamin Sparks <benjamin.sparks@protonmail.com>
2021-06-30 23:09:51 +0200

    test: update run all target

--- tests/run_tests.nim
+++ tests/run_tests.nim
@@ -4,6 +4,7 @@ import streams
 import test_operations_boundaries
 import test_operations_base
 import test_operations_strings
+import test_sqlite
 # import test_db
 # import test_script
 
@@ -15,5 +16,6 @@ addOutputFormatter(outputFormatter)
 test_boundaries()
 test_strings()
 test_base()
+test_sqlite()
 
 outputFormatter.close()
