3ed9fb3586786f73d2b2b0e12862505514e0f0af
Niklas Korz <niklas@niklaskorz.de>
2021-07-01 12:43:58 +0200

    Remove old db tests

--- tests/run_tests.nim
+++ tests/run_tests.nim
@@ -5,8 +5,6 @@ import test_operations_boundaries
 import test_operations_base
 import test_operations_strings
 import test_sqlite
-# import test_db
-# import test_script
 
 let resultFile = openFileStream("result.xml", fmWrite)
 
