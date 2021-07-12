3082692f1507c7f75ce80bbc92bdba58ca646f8f
Niklas Korz <niklas@niklaskorz.de>
2021-07-12 11:06:37 +0200

    Write tests for script execution of natural and nim syntax

--- tests/test_script.nim
+++ tests/test_script.nim
@@ -1,8 +1,17 @@
 import unittest
-import diesl/script
+import diesl/[script, operations]
 
 proc test_script*() =
   suite "script execution":
+    setup:
+      let schema = newDatabaseSchema({
+        "students": @{
+          "name": ddtString,
+          "firstName": ddtString,
+          "lastName": ddtString,
+        }
+      })
+
     test "simple script":
       let exportedOperations = runScript("""
 let x = 5
@@ -17,11 +26,126 @@ import sequtils
 """)
       check exportedOperations.len == 0
 
-    test "script with access to database":
+    test "script with access to database without schema checking":
       let exportedOperations = runScript("""
 db.students.name = db.students.name.trim(left)
 """)
-      check exportedOperations.len != 0
+      let expectedOperations = @[DieslOperation(
+        kind: dotStoreMany,
+        storeManyTable: "students",
+        storeManyColumns: @["name"],
+        storeManyValues: @[
+          DieslOperation(
+            kind: dotTrim,
+            trimValue: DieslOperation(
+              kind: dotLoad,
+              loadTable: "students",
+              loadColumn: "name",
+              loadType: ddtUnknown,
+            ),
+            trimDirection: left
+          )
+        ],
+        storeManyTypes: @[ddtUnknown]
+      )]
+      check $exportedOperations == $expectedOperations
+
+    test "script with access to database with schema checking":
+      let exportedOperations = runScript("""
+db.students.name = db.students.name.trim(left)
+""", schema)
+      let expectedOperations = @[DieslOperation(
+        kind: dotStoreMany,
+        storeManyTable: "students",
+        storeManyColumns: @["name"],
+        storeManyValues: @[
+          DieslOperation(
+            kind: dotTrim,
+            trimValue: DieslOperation(
+              kind: dotLoad,
+              loadTable: "students",
+              loadColumn: "name",
+              loadType: ddtString,
+            ),
+            trimDirection: left
+          )
+        ],
+        storeManyTypes: @[ddtString]
+      )]
+      check $exportedOperations == $expectedOperations
+
+    test "script with natural syntax working on whole table":
+      let exportedOperations = runScript("""
+change db.students:
+  trim beginning of name
+  replace "foo" with "bar" in name
+  take 1 to 3 from name
+""", schema)
+      let expectedOperations = @[DieslOperation(
+        kind: dotStoreMany,
+        storeManyTable: "students",
+        storeManyColumns: @["name"],
+        storeManyValues: @[
+          DieslOperation(
+            kind: dotSubstring,
+            substringValue: DieslOperation(
+              kind: dotReplace,
+              replaceValue: DieslOperation(
+                kind: dotTrim,
+                trimValue: DieslOperation(
+                  kind: dotLoad,
+                  loadTable: "students",
+                  loadColumn: "name",
+                  loadType: ddtString,
+                ),
+                trimDirection: left
+              ),
+              replaceTarget: lit"foo",
+              replaceReplacement: lit"bar"
+            ),
+            substringRange: 0..2
+          )
+        ],
+        storeManyTypes: @[ddtString]
+      )]
+      check $exportedOperations == $expectedOperations
+
+    test "script with natural syntax working on single column":
+      let exportedOperations = runScript("""
+change name of db.students:
+  trim beginning
+  replace "foo" with "bar"
+  take 1 to 3
+""", schema)
+      let expectedOperations = @[DieslOperation(
+        kind: dotStoreMany,
+        storeManyTable: "students",
+        storeManyColumns: @["name"],
+        storeManyValues: @[
+          DieslOperation(
+            kind: dotSubstring,
+            substringValue: DieslOperation(
+              kind: dotReplace,
+              replaceValue: DieslOperation(
+                kind: dotTrim,
+                trimValue: DieslOperation(
+                  kind: dotLoad,
+                  loadTable: "students",
+                  loadColumn: "name",
+                  loadType: ddtString,
+                ),
+                trimDirection: left
+              ),
+              replaceTarget: lit"foo",
+              replaceReplacement: lit"bar"
+            ),
+            substringRange: 0..2
+          )
+        ],
+        storeManyTypes: @[ddtString]
+      )]
+      check $exportedOperations == $expectedOperations
+
     test "script with a syntax error":
       expect ScriptExecutionError:
         discard runScript("""
