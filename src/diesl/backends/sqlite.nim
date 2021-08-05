29ae46404dbd98d393d58085e4edefe56777070d
Niklas Korz <niklas@niklaskorz.de>
2021-08-05 16:21:31 +0200

    Remove storeToCount from sqlite backend

--- src/diesl/backends/sqlite.nim
+++ src/diesl/backends/sqlite.nim
@@ -9,12 +9,12 @@ import ../extensions/sqlite
 
 import re
 
-proc toSqlite*(op: DieslOperation, storeToCount: int = 1): string {.gcSafe.} =
+proc toSqlite*(op: DieslOperation): string {.gcSafe.} =
   case op.kind:
     of dotStore:
-      fmt"UPDATE {op.storeTable} SET {op.storeColumn} = {op.storeValue.toSqlite(storeToCount)}"
+      fmt"UPDATE {op.storeTable} SET {op.storeColumn} = {op.storeValue.toSqlite}"
     of dotStoreMany:
-      let assignmentValues = op.storeManyValues.map((v) => v.toSqlite(storeToCount))
+      let assignmentValues = op.storeManyValues.map(toSqlite)
       let assignmentPairs = zip(op.storeManyColumns, assignmentValues)
       let assignments = assignmentPairs.map((pair) => [pair[0], pair[1]].join(" = ")).join(", ")
       fmt"UPDATE {op.storeManyTable} SET {assignments}"
@@ -33,33 +33,32 @@ proc toSqlite*(op: DieslOperation, storeToCount: int = 1): string {.gcSafe.} =
           "RTRIM"
         of TextDirection.both:
           "TRIM"
-      fmt"{trimFunction}({op.trimValue.toSqlite(storeToCount)})"
+      fmt"{trimFunction}({op.trimValue.toSqlite})"
     of dotSubstring:
-      fmt"SUBSTR({op.substringValue.toSqlite(storeToCount)}, {op.substringRange.a}, {op.substringRange.b})"
+      fmt"SUBSTR({op.substringValue.toSqlite}, {op.substringRange.a}, {op.substringRange.b})"
     of dotReplace:
-      fmt"REPLACE({op.replaceValue.toSqlite(storeToCount)}, {op.replaceTarget.toSqlite(storeToCount)}, {op.replaceReplacement.toSqlite(storeToCount)})"
+      fmt"REPLACE({op.replaceValue.toSqlite}, {op.replaceTarget.toSqlite}, {op.replaceReplacement.toSqlite})"
     of dotReplaceAll:
-      var value = op.replaceAllValue.toSqlite(storeToCount)
+      var value = op.replaceAllValue.toSqlite
       for pair in op.replaceAllReplacements:
-        value = fmt"REPLACE({value}, {pair.target.toSqlite(storeToCount)}, {pair.replacement.toSqlite(storeToCount)})"
+        value = fmt"REPLACE({value}, {pair.target.toSqlite}, {pair.replacement.toSqlite})"
       value
     of dotStringConcat:
-      fmt"{op.stringConcatValueA.toSqlite(storeToCount)} || {op.stringConcatValueB.toSqlite(storeToCount)}"
+      fmt"{op.stringConcatValueA.toSqlite} || {op.stringConcatValueB.toSqlite}"
     of dotToLower:
-      fmt"LOWER({op.toLowerValue.toSqlite(storeToCount)})"
+      fmt"LOWER({op.toLowerValue.toSqlite})"
     of dotToUpper:
-      fmt"UPPER({op.toUpperValue.toSqlite(storeToCount)})"
+      fmt"UPPER({op.toUpperValue.toSqlite})"
     of dotExtractOne:
-      fmt"extractOne({op.extractOneValue.toSqlite(storeToCount)}, '{op.extractOnePattern.pattern}')"
+      fmt"extractOne({op.extractOneValue.toSqlite}, '{op.extractOnePattern.pattern}')"
     of dotExtractMany:
-      assert(false, "Not implemented")
-      fmt"extractMany({op.extractManyValue.toSqlite(storeToCount)}, '{op.extractManyPattern.pattern}')"
+      fmt"extractMany({op.extractManyValue.toSqlite}, '{op.extractManyPattern.pattern}', {op.extractManyIndex})"
     of dotRegexReplace:
-      fmt"rReplace({op.regexReplaceValue.toSqlite(storeToCount)}, {op.regexReplaceTarget.toSqlite(storeToCount).pattern}, {op.regexReplaceReplacement.toSqlite(storeToCount)})"
+      fmt"rReplace({op.regexReplaceValue.toSqlite}, {op.regexReplaceTarget.toSqlite.pattern}, {op.regexReplaceReplacement.toSqlite})"
     of dotRegexReplaceAll:
-      var value = op.replaceAllValue.toSqlite(storeToCount)
+      var value = op.replaceAllValue.toSqlite
       for pair in op.regexReplaceAllReplacements:
-        value = fmt"rReplace({value}, {pair.target.toSqlite(storeToCount).pattern}, {pair.replacement.toSqlite(storeToCount)})"
+        value = fmt"rReplace({value}, {pair.target.toSqlite.pattern}, {pair.replacement.toSqlite})"
       value
 
 
@@ -67,13 +66,7 @@ proc toSqlite*(operations: seq[DieslOperation]): seq[SqlQuery] {.gcSafe.} =
   var queries: seq[SqlQuery]
   for operation in operations:
     assert operation.kind == dotStore or operation.kind == dotStoreMany
-    let query = SqlQuery(
-      if operation.kind == dotStore:
-        operation.toSqlite(1)
-      else:
-        operation.toSqlite(operation.storeManyColumns.len)
-    )
-    queries.add(query)
+    let query = operation.toSqlite()
+    queries.add(SqlQuery(query))
   return queries
-
 
