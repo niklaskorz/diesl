093b5c3cd5e7cb61f4afd9568f0a8228e5ea9fec
Niklas Korz <niklas@niklaskorz.de>
2021-09-04 08:58:36 +0200

    Beautiful formatting for demo

--- src/diesl.nim
+++ src/diesl.nim
@@ -11,10 +11,10 @@ export sqliteviews
 when isMainModule:
   # Standalone demo
   import os
-  import tables
   import parsecsv
   import streams
   import db_sqlite
+  import terminaltables
   import diesl/operations
   import diesl/extensions/sqlite as sqliteextensions
 
@@ -22,8 +22,8 @@ when isMainModule:
     echo "Usage: " & paramStr(0) & " <mode> <scriptname>"
     quit(QuitFailure)
   let mode = paramStr(1)
-  if mode != "direct" and mode != "view":
-    echo "Mode must be one of 'direct' or 'view'"
+  if mode != "direct" and mode != "views":
+    echo "Mode must be one of 'direct' or 'views'"
     quit(QuitFailure)
   let scriptName = paramStr(2)
 
@@ -50,13 +50,18 @@ when isMainModule:
     sql"CREATE TABLE students (name TEXT, firstName TEXT, secondName TEXT, age INT, email TEXT, data TEXT)"
   )
 
+  # Terminal table for formatted output
+  let outputTable = newUnicodeTable()
+
   # Populate with example data
   var exampleDataFile = "examples/data.csv"
   var exampleDataStream = newFileStream(exampleDataFile, fmRead)
   var csvParser: CsvParser
   csvParser.open(exampleDataStream, exampleDataFile)
-  # Skip header row
-  discard readRow(csvParser)
+  # Header row
+  if readRow(csvParser):
+    outputTable.setHeaders(csvParser.row[1..^1])
+  # Data rows
   while readRow(csvParser):
     dbConn.exec(
       sql"INSERT INTO students (name, firstName, secondName, age, email, data) VALUES (?, ?, ?, ?, ?, ?)",
@@ -64,29 +69,38 @@ when isMainModule:
     )
   csvParser.close()
 
+  echo ""
+
   if mode == "direct":
     let queries = exportedOperations.toSqlite()
     echo "Generated queries:"
+    echo ""
     for query in queries:
       echo string(query)
       let preparedStatement = dbConn.prepare(string(query))
       dbConn.exec(preparedStatement)
       preparedStatement.finalize()
+    echo ""
     echo "Final data:"
+    echo ""
     for row in dbConn.fastRows(sql"SELECT * FROM students"):
-      echo row
-  elif mode == "view":
+      outputTable.addRow(row)
+    printTable(outputTable)
+  elif mode == "views":
     let (queries, tableAccessMap, views) = exportedOperations.toSqliteViews(schema)
+    discard views
     echo "Generated queries:"
+    echo ""
     for query in queries:
       echo string(query)
       let preparedStatement = dbConn.prepare(string(query))
       dbConn.exec(preparedStatement)
       preparedStatement.finalize()
-    echo "Table access map: ", $tableAccessMap
-    echo "Views: ", $views
+    echo ""
     echo "Final data:"
+    echo ""
     for row in dbConn.fastRows(sql"SELECT * FROM ?", tableAccessMap.getTableAccessName("students")):
-      echo row
+      outputTable.addRow(row)
+    printTable(outputTable)
 
   dbConn.close()
